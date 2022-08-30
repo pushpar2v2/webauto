2957f6fb12ede80954f6aca453f3678f5009c281
pushpar2v2 <pushpa.kamaraj@r2v2.in>
2022-08-30 14:07:26 +0530

    test1

---
+++ Test Suites/prod.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>prod</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>998500b7-4fba-4faa-80df-750efabcb993</testSuiteGuid>
+</TestSuiteEntity>
